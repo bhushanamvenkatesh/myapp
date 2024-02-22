0445ef867990b00efb270dc6b258d06dd229888f
bhushanamvenkatesh <venkat7596@gmail.com>
2024-02-22 07:10:11 +0530

    two-way-data-binding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 // import logo from './logo.svg';
 // import React, {useState} from 'react';
 import Products from './Products'
-
+// import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  
+  let [val, setVal] = useState(100)
+  function update(e) {
+    setVal(e.target.value)
+    console.log(val)
+  }
+
   return (
-   <Products/>
+    <>
+      <h1>{val}</h1>
+      <button onClick={() => {
+
+        setVal(val + 100)
+
+      }
+      }>change</button>
+      <input type='text' onChange={(event) => {
+        update(event)
+      }} value={val} />
+
+    </>
+
   );
 }
 
