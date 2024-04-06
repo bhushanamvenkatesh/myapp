dda8476ebf5203d7208dbe1f93eb9ddff408360f
bhushanamvenkatesh <venkat7596@gmail.com>
2024-04-06 08:28:30 +0530

    boiler plate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-// import logo from './logo.svg';
-// import React, {useState} from 'react';
-import Products from './Products'
-
 import './App.css';
 
 function App() {
   
   return (
-   <Products/>
+   <h>hello</h>
   );
 }
 
